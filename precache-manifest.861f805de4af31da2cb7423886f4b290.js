a6015849faf9c83066bf9dbc26a7ad76d101e0ee
Maksi Bajo <maksimiliano.bajo@nebula.al>
2022-03-30 17:10:28 +0200

    Updates

---
+++ precache-manifest.861f805de4af31da2cb7423886f4b290.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c07b42c4e5c43bffd5fe9c094ba8b8f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6580152c5739a1bfc458",
+    "url": "./static/css/main.ddbb9cf4.chunk.css"
+  },
+  {
+    "revision": "a48841786b94de681076",
+    "url": "./static/js/2.a0968456.chunk.js"
+  },
+  {
+    "revision": "72c2168a992bae65033dc2d74bcc3eb6",
+    "url": "./static/js/2.a0968456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6580152c5739a1bfc458",
+    "url": "./static/js/main.d42561ca.chunk.js"
+  },
+  {
+    "revision": "20434448d009394373fe",
+    "url": "./static/js/runtime-main.ea6b22e0.js"
+  },
+  {
+    "revision": "8e33e812137ef5c70475fb8b2c696a19",
+    "url": "./static/media/POK~ODaiIRFl.8e33e812.svg"
+  },
+  {
+    "revision": "0e3908536c98063cffb9db1e51906431",
+    "url": "./static/media/appleStore~kiIWxpEr.0e390853.svg"
+  },
+  {
+    "revision": "4c56f15332c59472a56a5c554c052434",
+    "url": "./static/media/googlePlay~CRBRzWtN.4c56f153.svg"
+  },
+  {
+    "revision": "e3f5977fd65b994fb0d61cd1c97dd536",
+    "url": "./static/media/header~chxtzlIq.e3f5977f.svg"
+  },
+  {
+    "revision": "5f4595605108dd216ba78f83c4e7d51a",
+    "url": "./static/media/phone~xUdzTTki.5f459560.svg"
+  },
+  {
+    "revision": "efda42444e3613d337d8a1923111a4cd",
+    "url": "./static/media/scanAndPayText~xLhgnkiz.efda4244.svg"
+  }
+]);
